2af676951de03cdc2c928a7c3853e4d3a74711a3
CHIMITA <smile0375-@naver.com>
2022-12-14 11:33:19 +0900

    main.js modified

--- Sec_Vill/JS/main.js
+++ Sec_Vill/JS/main.js
@@ -1,43 +1,26 @@
-// 모달 열기
-function modalOpen() {
-    document.querySelector('.modal_wrap').style.display = 'block';
-    document.querySelector('.modal_background').style.display = 'block';
+
+function send(message){
+
+    const element = document.getElementById('chat');
+    //const content = document.createTextNode(message);
+
+    // new div 생성
+    const newP = document.createElement('p');
+   
+    // new div에 추가할 text node 생성
+    const newText = document.createTextNode(message);
+  
+    // text node를 new div에 추가
+    newP.appendChild(newText);
+  
+    // new div를 기존 div에 추가
+    element.appendChild(newP);
+    
+    document.querySelector("#chat_box").append(newP);
+
+  
 }
 
-// 모달 끄기
-function modalClose() {
-    document.querySelector('.modal_wrap').style.display = 'none';
-    document.querySelector('.modal_background').style.display = 'none';
+function loading(){
+    location.href = "../Html/my_vil.html";
 }
-
-
-//버튼 클릭리스너 달기
-document.querySelector('#modal_btn').addEventListener('click', modalOpen);
-document.querySelector('.modal_close').addEventListener('click', modalClose);
-
-//login
-/*
-function login_check() {
-    const ID_s = 'admin';
-    const PW_s = 'admin1234';
-
-    var id = document.getElementById('ID').value;
-    var pw = document.getElementById('PW').value;
-    var name = document.getElementById('name').value;
-
-    if (ID_s === id && PW_s === pw){
-        alert("로그인 성공" + name + "님 환영합니다!");
-        location.href = "../Html/main.html";
-    }
-    else {
-        alert("ID와 PW 가 맞지 않습니다. 다시 시도해주세요 :D");
-        location.href = "../Html/login.html";
-    }
-
-}
-*/
-
-function input_box() {
-
-}
-
